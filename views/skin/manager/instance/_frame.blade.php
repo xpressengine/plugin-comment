7a022adb6ea20cea64f36a4d5c512f7cff242eaa
sirwoongke <fzwoongbin@naver.com>
2019-06-11 18:04:47 +0900

    사용하지 않는 관리자 페이지 빵조각 추가 소스 삭제

--- views/skin/manager/instance/_frame.blade.php
+++ views/skin/manager/instance/_frame.blade.php
@@ -6,10 +6,6 @@
     <small>{{ xe_trans('comment::manage.detailSettingDesc') }}</small>
 @endsection
 
-@section('content_bread_crumbs')
-    <a href="{{ XeMenu::getInstanceSettingURI($menuItem) }}"><i class="xi-arrow-left"></i> {{xe_trans($menuItem->title)}}</a>
-@endsection
-
 <ul class="nav nav-tabs">
     <li @if($_active == 'config') class="active" @endif><a href="{{ route('comment::setting.config', $targetInstanceId) }}">{{xe_trans('comment::manage.detailSetting')}}</a></li>
     <li @if($_active == 'perm') class="active" @endif><a href="{{ route('comment::setting.perm', $targetInstanceId) }}">{{xe_trans('xe::permission')}}</a></li>
