0124dd8c110a13f955697a4161a1be2d275d706e
mjae404 <mjae404@gmail.com>
2020-09-07 18:15:41 +0900

    새스킨 작업

--- views/skin/user/newDefault/container.blade.php
+++ views/skin/user/newDefault/container.blade.php
@@ -1,31 +1,33 @@
-{{ XeFrontend::css('plugins/comment/assets/css/new-comment.css')->load() }}
+{{ XeFrontend::css('plugins/comment/assets/css/xe-comment-default.css')->load() }}
 
-<div class="xe-list-comment">
-    <div class="xe-list-comment--header">
-        <div class="xe-list-comment--header-left-box">
-            <span class="xe-list-comment--header-count-number __xe_comment_cnt">0</span><span class="xe-list-comment--header-count-text">{{ xe_trans('xe::ea') }} {{ xe_trans('xe::comment') }}</span>
+<div class="xf-comment-box">
+    <div class="xf-comment-title-box">
+        <div class="xf-comment-title">
+            <span class="xf-comment-number __xe_comment_cnt">0</span>
+            <span class="xf-comment-title-text">{{ xe_trans('xe::ea') }} {{ xe_trans('xe::comment') }}</span>
         </div>
     </div>
 
     @if($config->get('reverse') === true)
         <div class="__xe_comment_form"></div>
 
-        <div class="xe-list-comment--body">
-            <ul class="xe-list-comment--list __xe_comment_list comment_list"></ul>
+        <div class="xf-comment-contents __xe_comment_list comment_list">
+
         </div>
-    
+
         <div class="pagination_seemore __xe_comment_btn_more" style="display: none;">
-            <a href="#">{{ xe_trans('comment::viewMore') }} (<span class="__xe_comment_remain_cnt">0</span>{{ xe_trans('comment::remainCount') }})</a>
+            <a href="#">{{ xe_trans('comment::viewMore') }} (<span
+                    class="__xe_comment_remain_cnt">0</span>{{ xe_trans('comment::remainCount') }})</a>
         </div>
     @else
         <div class="pagination_seemore __xe_comment_btn_more" style="display: none;">
-            <a href="#">{{ xe_trans('comment::viewMore') }} (<span class="__xe_comment_remain_cnt">0</span>{{ xe_trans('comment::remainCount') }})</a>
+            <a href="#">{{ xe_trans('comment::viewMore') }} (<span
+                    class="__xe_comment_remain_cnt">0</span>{{ xe_trans('comment::remainCount') }})</a>
         </div>
 
-        <div class="xe-list-comment--body">
-            <ul class="xe-list-comment--list __xe_comment_list comment_list"></ul>
-        </div>
+        <div class="xf-comment-contents __xe_comment_list comment_list">
 
+        </div>
         <div class="__xe_comment_form"></div>
     @endif
 </div>
--- views/skin/user/newDefault/items.blade.php
+++ views/skin/user/newDefault/items.blade.php
@@ -1,119 +1,131 @@
 <?php
+
 use Xpressengine\Plugins\Comment\Models\Comment;
 use Xpressengine\User\Rating;
+
 ?>
 
 @foreach($items as $item)
-    <li class="xe-list-comment--list-item @if ($item->getDepth() > 0) xe-list-comment--reply-list-item @endif comment_entity depth{{ $item->getDepth() }} __xe_comment_list_item" id="#comment-{{ $item->id }}" data-instance_id="{{ $item->instance_id }}" data-id="{{ $item->id }}" data-head="{{ $item->head }}" data-reply="{{ $item->reply }}" data-parent_id="{{ $item->parent_id }}" data-indent="{{ $item->getDepth() }}">
-        <div class="xe-list-comment--list-item-wrapper">
-            <div class="xe-list-comment-list--left-box">
-                <a href="#" class="xe-list-comment__link">
-                    <span class="xe-list-comment-list__user-image" style="background-image: url({{ $item->getAuthor()->getProfileImage() }})"><span class="blind">유저 이미지</span></span>
-                </a>
-            </div>
-            <div class="xe-list-comment-list--right-box">
-                <div class="xe-list-comment-list--title">
-                    <div class="xe-list-comment-list--left-box">
-                        @if ($item->author != null)
-                            <span class="xe-list__display_name">
-                                <a href="{{ sprintf('/@%s', $item->user_id) }}" class="xe-list-comment__link {{ $item->getAuthor()->getRating() !== Rating::GUEST ? '__xe_user' : '' }}"
-                                   data-toggle="xe-page-toggle-menu"
-                                   data-url="{{ route('toggleMenuPage') }}"
-                                   data-data='{!! json_encode(['id'=>$item->user_id, 'type'=>'user']) !!}'>{{ $item->writer }}</a>
-                           </span>
-                        @else
-                            <span class="xe-list__display_name">{{ $item->writer }}</span>
-                        @endif
-                        
-                        <span class="xe-list-item___detail xe-list-item___detail-read_count xe-list__mobile-style">
-                            <span class="xe-list-item___detail-label">{{ $item->created_at->format('Y.m.d. H:i:s') }}</span>
-                        </span>
-                    </div>
-                    <div class="xe-list-comment-list--right-box">
-                        <div class="xe-list-body__edit-box">
-                            @can('update-visible', $item)
-                                <span class="xe-list-body__edit-item xe-list-body__edit"><a href="#" class="xe-list-comment-list__link comment_modify __xe_comment_btn_edit">수정</a></span>
-                            @endcan
-                            @can('delete-visible', $item)
-                                <span class="xe-list-body__edit-item xe-list-body__delete"><a href="#" class="xe-list-comment-list__link comment_delete __xe_comment_btn_destroy">삭제</a></span>
-                            @endcan
-                        </div>
-                        <div class="xe-list__icon-box">
-                            <span class="xe-list__icon xe-list__more"><a href="#" class="comment_more_view xe-list-comment-list__link" data-toggle="xe-page-toggle-menu" data-url="{{route('toggleMenuPage')}}" data-data='{!! json_encode(['id'=>$item->id,'type'=>'comment', 'instanceId'=>$item->instance_id]) !!}' data-side="dropdown-menu-right"><i class="xi-ellipsis-h"></i></a></span>
-                        </div>
-                    </div>
+    <div class="xf-comment-item xf-comment __xe_comment_list_item @if ($item->getDepth() > 0) xf-comment-reply @endif"
+         id="#comment-{{ $item->id }}" data-instance_id="{{ $item->instance_id }}" data-id="{{ $item->id }}"
+         data-head="{{ $item->head }}" data-reply="{{ $item->reply }}" data-parent_id="{{ $item->parent_id }}"
+         data-indent="{{ $item->getDepth() }}">
+        <div class="xf-profile-img-box">
+            <div class="xf-profile-img"
+                 style="background-image: url('/assets/core/user/img/default_avatar.jpg')"></div>
+        </div>
+        <div class="xf-comment-contents-box">
+            <div class="xf-comment-info-box">
+                <div class="xf-profile-box">
+                    <span class="xf-comment-nickname">{{ $item->writer }}</span>
+                    <span class="xf-comment-date">{{ $item->created_at->format('Y.m.d') }}</span>
                 </div>
-                <div class="xe-list-comment-list--body-text">
-{{--                    TODO 비밀 아이콘 스타일--}}
-                    @if($item->display == Comment::DISPLAY_SECRET)
-                        <span class="bd_ico_lock"><i class="xi-lock"></i><span class="xe-sr-only">secret</span></span>
-                    @endif
-                    <p class="xe-list-comment-list--body-text-paragraph">
-                        @can('read', $item)
-                            {!! compile($item->instance_id, $item->getContent(), $item->format === Comment::FORMAT_HTML) !!}
-                        @else
-                            @if($item->display == Comment::DISPLAY_SECRET)
-                                {{ xe_trans('comment::secretContent') }}
-                            @else
-                                {{ xe_trans('comment::NotAllowContent') }}
-                            @endif
+                <div class="xf-comment-edit-box xf-list">
+                    <ul class="xf-edit-list xf-list">
+                        @can('update-visible', $item)
+                            <li class="xf-edit-item">
+                                <a href="#" class="xf-edit__link xf-a __xe_comment_btn_edit">수정</a>
+                            </li>
                         @endcan
+                        @can('delete-visible', $item)
+                            <li class="xf-edit-item">
+                                <a href="#" class="xf-edit__link xf-a __xe_comment_btn_destroy">삭제</a>
+                            </li>
+                        @endcan
+                    </ul>
+                    <div class="xf-report-box">
+                        <button type="button" class="xf-report-icon xf-comment-btn" data-toggle="xe-page-toggle-menu"
+                                data-url="{{route('toggleMenuPage')}}"
+                                data-data='{!! json_encode(['id'=>$item->id,'type'=>'comment', 'instanceId'=>$item->instance_id]) !!}'
+                                data-side="dropdown-menu-right">
+                            <i class="xi-ellipsis-h"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="xf-comment-text-box">
+                <p class="xf-p xf-comment-text">
+                @if($item->display === Comment::DISPLAY_SECRET)
+                    <div class="xf-secret-icon">
+                        <span class="xe-sr-only">secret</span>
+                    </div>
 
-                        @foreach($fieldTypes as $fieldType)
-                            {!! $fieldType->getSkin()->show($item->getAttributes()) !!}
+                @endif
+
+                @can('read', $item)
+                    {!! compile($item->instance_id, $item->getContent(), $item->format === Comment::FORMAT_HTML) !!}
+
+                    @foreach($fieldTypes as $fieldType)
+                        {!! $fieldType->getSkin()->show($item->getAttributes()) !!}
+                    @endforeach
+                @else
+                    @if($item->display == Comment::DISPLAY_SECRET)
+                        <span class="xf-secret-text">
+                            {{ xe_trans('comment::secretContent') }}
+                        </span>
+                        @else
+                        {{ xe_trans('comment::NotAllowContent') }}
+                        @endif
+                        @endcan
+                        </p>
+            </div>
+
+            @if(count($item->files) > 0)
+                <div class="xf-comment-file-box">
+                    <!-- [D] 클릭시 클래스 on 적용 -->
+                    <a href="#" class="xf-comment-file__toggle __xe_comment_btn_toggle_file xf-a">
+                        <span class="xf-comment-file-text">첨부파일</span>
+                        <strong class="xf-comment-file__number">{{ count($item->files) }}</strong>
+                    </a>
+                    <ul class="xf-comment-file-list xf-list">
+                        @foreach($item->files as $file)
+                            <li class="xf-comment-file-item">
+                                <a href="{{ route('editor.file.download', ['instanceId' => $item->instance_id, 'fileId' => $file->id]) }}"
+                                   class="xf-a xf-comment-file__link">
+                                    <i class="xi-download"></i> {{ $file->clientname }} <span
+                                        class="xf-comment-file__size">({{ bytes($file->size) }})</span>
+                                </a>
+                            </li>
                         @endforeach
-                    </p>
+                    </ul>
                 </div>
-                <div class="xe-list-comment-list--more-info">
-{{--                    TODO 첨부파일 스타일 필요--}}
-                    @if(count($item->files) > 0)
-                        <div class="comment_file_list">
-                            <!-- [D] 클릭시 클래스 on 적용 -->
-                            <a href="#" class="btn_file __xe_comment_btn_toggle_file">첨부파일 <strong class="file_num">{{ count($item->files) }}</strong></a>
-                            <ul>
-                                @foreach($item->files as $file)
-                                    <li>
-                                        <a href="{{ route('editor.file.download', ['instanceId' => $item->instance_id, 'fileId' => $file->id]) }}">
-                                            <i class="xi-download"></i> {{ $file->clientname }} <span class="file_size">({{ bytes($file->size) }})</span>
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    
-                    @if ($config->get('useAssent') === true)
-                        <div class="xe-list-comment-list__box-assent_count xe-list-comment-list__more-info-item">
-                            <span class="blind">추천</span>
-                            <a href="#" class="xe-list-comment-list__link btn_share like __xe_comment_btn_vote __xe_assent @if($item->isAssented()) on @endif">
-                                <img src="{{ url('plugins/comment/assets/img/assent.svg') }}" alt="추천 아이콘" class="xe-list-comment-list-img">
-                            </a>
-                            <span class="xe-list-comment-list__assent_count">
-                                <a href="#" class="xe-list-comment-list__link btn_share like_num __xe_comment_count __xe_assent">{{ number_format($item->assent_count) }}</a>
-                            </span>
-                        </div>
-                    @endif
-                    
-                    @if ($config->get('useDissent') === true)
-                        <div class="xe-list-comment-list__box-dissent_count xe-list-comment-list__more-info-item">
-                            <span class="blind">비추천</span>
-                            <a href="#" class="xe-list-comment-list__link btn_share dissent __xe_comment_btn_vote __xe_dissent @if($item->isDissented()) on @endif">
-                                <img src="{{ url('plugins/comment/assets/img/dissent.svg') }}" alt="비추천 아이콘" class="xe-list-comment-list-img">
-                            </a>
-                            <span class="xe-list-comment-list__dissent_count">
-                                <a href="#" class="xe-list-comment-list__link btn_share dissent_num __xe_comment_count __xe_dissent">{{ number_format($item->dissent_count) }}</a>
-                            </span>
-                        </div>
-                    @endif
-                    @can('create', $instance)
-                        <div class="xe-list-comment-list__more-info-item">
-                            <span><a href="#" class="xe-list-comment__link btn_share reply __xe_comment_btn_reply">답글달기</a></span>
-                        </div>
-                    @endcan
-                    <div class="vote_list __xe_comment_voters __xe_assent __xe_dissent"></div>
-                </div>
+            @endif
+            <div class="xf-community-box">
+                @if ($config->get('useAssent') === true || $config->get('useDissent') === true)
+                    <div class="xf-assent-box">
+                        <ul class="xf-assent-list xf-list">
+                            @if ($config->get('useAssent') === true)
+                                <li class="xf-assent-item">
+                                    <button type="button"
+                                            class="xf-assent-btn xf-comment-btn __xe_comment_btn_vote __xe_assent @if($item->isAssented()) on @endif">
+                                        <div class="xf-assent xf-assent-icon"></div>
+                                        <div
+                                            class="xf-assent-text __xe_comment_count __xe_assent">{{ number_format($item->assent_count) }}</div>
+                                    </button>
+                                </li>
+                            @endif
+
+                            @if ($config->get('useDissent') === true)
+                                <li class="xf-assent-item">
+                                    <button type="button"
+                                            class="xf-assent-btn xf-comment-btn __xe_comment_btn_vote __xe_dissent @if($item->isDissented()) on @endif">
+                                        <div class="xf-dissent xf-assent-icon"></div>
+                                        <div
+                                            class="xf-assent-text __xe_comment_count __xe_dissent">{{ number_format($item->dissent_count) }}</div>
+                                    </button>
+                                </li>
+                            @endif
+                        </ul>
+                    </div>
+                @endif
+                @can('create', $instance)
+                    <div class="xf-reply-btn-box">
+                        <button type="button" class="xf-reply-btn xf-comment-btn __xe_comment_btn_reply">
+                            <span class="xf-reply__text">답글달기</span>
+                        </button>
+                    </div>
+                @endcan
             </div>
         </div>
-        <div class="__xe_comment_edit_form __xe_comment_reply_form __xe_comment_certify"></div>
-    </li>
+    </div>
 @endforeach
