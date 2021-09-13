a69cd79a8c63d69876696a1a84eb025c2bbf7c06
Bi Kim <kimbi@outlook.kr>
2021-09-13 10:16:34 +0900

    (#86) 복수 코멘트 렌더링 해결

--- assets/js/service.js
+++ assets/js/service.js
@@ -69,6 +69,7 @@
 
         var dom = $.parseHTML(data.html)
         var form = new CommentForm(dom, 'create', function (data) {
+          that.items = []
           that._assetLoad(data.XE_ASSET_LOAD)
 
           var items = that.makeItems($.parseHTML(data.items, document, true))
